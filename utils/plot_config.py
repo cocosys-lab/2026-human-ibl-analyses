47914676fef359b46891b1e3579602d2633be967
phijoh <pajohnson@student.unimelb.edu.au>
2026-09-24 15:18:31 +0200

    add extra cols for fig 3

--- utils/plot_config.py
+++ utils/plot_config.py
@@ -15,12 +15,15 @@ COLORS = {
     "instructions": "mediumturquoise",
     "no_instructions": "palevioletred",
     'correct': 'darkgreen',
-    'incorrect': 'firebrick'
+    'incorrect': 'firebrick',
+    'cursor_early_trial': 'yellow',
+    'cursor_late_trial': 'limegreen'
 }
 
 # Ordered palettes
 CONTINUOUS_PALETTE = sns.color_palette("viridis", as_cmap=True)
 DIVERGING_PALETTE = sns.color_palette("coolwarm", as_cmap=True)
+CONTRAST_PALETTE = sns.color_palette('Greys', n_colors=5)
 
 # Font sizes, centralized
 FONT_SIZES = {
