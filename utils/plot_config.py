571bcfc7a8192b727b4a3acf700b8ff544c3bcf7
phijoh <pajohnson@student.unimelb.edu.au>
2026-09-25 16:25:07 +0200

    update contrast palette

--- utils/plot_config.py
+++ utils/plot_config.py
@@ -23,7 +23,7 @@ COLORS = {
 # Ordered palettes
 CONTINUOUS_PALETTE = sns.color_palette("viridis", as_cmap=True)
 DIVERGING_PALETTE = sns.color_palette("coolwarm", as_cmap=True)
-CONTRAST_PALETTE = sns.color_palette('Greys', n_colors=5)
+CONTRAST_PALETTE = sns.color_palette('Greys', n_colors=10)[-5:]
 
 # Font sizes, centralized
 FONT_SIZES = {
