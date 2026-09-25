8783f991e587dbef77adcbdab27b96afef763c06
phijoh <pajohnson@student.unimelb.edu.au>
2026-09-25 15:04:38 +0200

    parse cursorTime to array of floats; delete extra cursor samples beyond response time out

--- src/2_process_data.py
+++ src/2_process_data.py
@@ -41,14 +41,24 @@ processed_df['sideContrast'] = processed_df['contLeft'] - processed_df['contRigh
 # processed_df.loc[(processed_df['contrastLeft']==0.5)&(processed_df['contrastRight']==0.5), 'contRight'] = np.nan
 
 # get the stimulus trajectory and convert to float
-processed_df['stimTrajectory'] = None
+# N.B. The cursor position could be recorded for up to 506ms after the response window time out at 10s. This was always registered as a no-response and feedback was given accordingly, even if the cursor crossed the response threshold in the extra samples. We therefore remove the extra samples here.
+processed_df['cursorPosition'] = None
+processed_df['cursorTime'] = None
+not_same_len = []
+discrepancy = []
 for idx in processed_df.index:
     mouse_str = processed_df.loc[idx, 'dot_trajectory'][1:-1]
-    mouse_float = [float(n) for n in mouse_str.split(",")]
-    processed_df.at[idx, 'stimTrajectory'] = mouse_float
+    mouse_float = np.array([float(n) for n in mouse_str.split(",")])
+    processed_df.at[idx, 'cursorPosition'] = mouse_float
+    time_str = processed_df.loc[idx, 'mouse.time'][1:-1]
+    time_float = np.array([float(n) for n in time_str.split(",")])
+    processed_df.at[idx, 'cursorTime'] = time_float
+    if len(mouse_float) > len(time_float):
+        processed_df.at[idx, 'response_time_from_stim'] = np.nan
+        processed_df.at[idx, 'cursorPosition'] = mouse_float[:len(time_float)]
 
 # only keep columns we need to avoid confusion
-processed_df = processed_df[['subject', 'session', 'trial', 'probabilityLeft', 'contLeft', 'contRight', 'choice', 'firstMovement_times_from_stim', 'response_times_from_stim', 'feedbackType', 'stimContrast', 'sideContrast', 'stimSide', 'instructions', 'stimTrajectory', 'mouse.time', 'dot.started']]
+processed_df = processed_df[['subject', 'session', 'trial', 'probabilityLeft', 'contLeft', 'contRight', 'choice', 'firstMovement_times_from_stim', 'response_times_from_stim', 'feedbackType', 'stimContrast', 'sideContrast', 'stimSide', 'instructions', 'cursorPosition', 'cursorTime', 'dot.started']]
 processed_df.rename(columns={'contLeft':'contrastLeft', 'contRight':'contrastRight'}, inplace=True)
 
 # save csv
